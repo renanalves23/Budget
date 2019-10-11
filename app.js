9f7e1fbe97000cf7c56d37fbb45593695f42b401
renan <renanalves23@outlook.com>
2019-10-11 13:55:14 -0300

    Chore: eventListener keypress keycode13 = ENTER

--- app.js
+++ app.js
@@ -13,23 +13,38 @@ var UIController = (function() {
 
 })();
 
+
 // GLOBAL APP CONTROLLER
 // Terceiro module criado para que possa passar as informações
 //*****passando os outros 2 módulos como parâmetros para que saiba da existencia dos outros 2 e possam se conectar
 var controller = (function(budgetCtrl, UICtrl) { 
 
-  document.querySelector('.add__btn').addEventListener('click', function() {
-    // 1. get the field input data
+    //custom function
+    var ctrlAddItem = function() {
 
-    //2. add the item to the budget controller
+        // 1. get the field input data
 
-    //3. add the item to the UI
+        //2. add the item to the budget controller
 
-    //4. Calculate the budget
+        //3. add the item to the UI
 
-    //5.Display the budget on the UI
+        //4. Calculate the budget
 
-  })
+        //5.Display the budget on the UI
+      window.alert('CHESSUS')
+    }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  //KEYPRESS EVENT
+  document.addEventListener('keypress', function(event) {
+      //caso seja pressionado a Tecla ENTER
+    if (event.keyCode === 13 || event.wich === 13) { //.keyCode para browsers modernos && .wich para browsers antigos
+        ctrlAddItem();
+    }
+
+  });
+
 
 })(budgetController, UIController);//******/
 
